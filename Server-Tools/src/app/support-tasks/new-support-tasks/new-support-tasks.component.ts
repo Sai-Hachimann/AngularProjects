0d60dfa12c049ef4b9c31c623449c872544092b9
sai <vsaisumanth241196@gmail.com>
2024-09-19 20:33:05 +0530

    some added reusability

--- Server-Tools/src/app/support-tasks/new-support-tasks/new-support-tasks.component.ts
+++ Server-Tools/src/app/support-tasks/new-support-tasks/new-support-tasks.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-support-tasks',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './new-support-tasks.component.html',
   styleUrl: './new-support-tasks.component.css'
 })
