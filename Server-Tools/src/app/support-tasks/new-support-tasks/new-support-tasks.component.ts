02660ac021cc5928c17c99fcdf5bd7573d8a0800
sai <vsaisumanth241196@gmail.com>
2024-09-20 14:30:16 +0530

    added more reusable components

--- Server-Tools/src/app/support-tasks/new-support-tasks/new-support-tasks.component.ts
+++ Server-Tools/src/app/support-tasks/new-support-tasks/new-support-tasks.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
-
+import { ControlComponent } from '../../shared/control/control.component';
 @Component({
   selector: 'app-new-support-tasks',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ControlComponent],
   templateUrl: './new-support-tasks.component.html',
-  styleUrl: './new-support-tasks.component.css'
+  styleUrl: './new-support-tasks.component.css',
 })
-export class NewSupportTasksComponent {
-
-}
+export class NewSupportTasksComponent {}
